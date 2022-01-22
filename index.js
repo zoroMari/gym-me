908e4ab144e06cf7c5d7c8158a6ed5cef310cd0a
zoroMari <98117895+zoroMari@users.noreply.github.com>
2022-01-22 16:44:32 +0200

    Create actual master (#5)
    * changes in article FAQ (#2)
    
    * Feature/overflow x (#3)
    
    * Comments overflow-x:hidden
    
    * Comments overflow-x:hidden
    
    * Article Training programs done
    
    * Change size of Comment block on mobile
    
    * Feature/validation of event target in FAQ #4

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const questions = document.querySelector('.Questions');
 function handleQuestionsToggle(event) {
   const questionWithAnswer = event.target.closest('.QuestionWithAnswer'); 
 
+  if (!questionWithAnswer) return;
+
   const question = questionWithAnswer.querySelector('.Question');
   question.classList.toggle('Question_open');
 
   const answer = questionWithAnswer.querySelector('.Answer');
   answer.classList.toggle('Answer_open');
 
-  console.log('event.target >>>', event.target);
 }   
 
 questions.addEventListener('click', handleQuestionsToggle);
