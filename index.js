d11915d5b455fe0f161ac62736ea64c249ec7bb7
Mariia Moroz <mashanya.kov@gmail.com>
2022-01-24 19:00:38 +0200

    Feature/Go up button

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const questions = document.querySelector('.Questions');
+function questionsToggle() {
+  const questions = document.querySelector('.Questions');
+
+  function handleQuestionsToggle(event) {
+    const questionWithAnswer = event.target.closest('.QuestionWithAnswer'); 
+
+    if (!questionWithAnswer) return;
+
+    const question = questionWithAnswer.querySelector('.Question');
+    question.classList.toggle('Question_open');
+
+    const answer = questionWithAnswer.querySelector('.Answer');
+    answer.classList.toggle('Answer_open');
+  }   
+
+  questions.addEventListener('click', handleQuestionsToggle);
+}
+
+questionsToggle();
 
 
-function handleQuestionsToggle(event) {
-  const questionWithAnswer = event.target.closest('.QuestionWithAnswer'); 
+function goUpButton() {
+  const goUpButton = document.getElementById('goUp');
 
-  if (!questionWithAnswer) return;
+  function handleShowButton() {
+    pageYOffset > document.documentElement.clientHeight ? goUpButton.style.display = 'block' :
+    goUpButton.style.display = '';
+  }
 
-  const question = questionWithAnswer.querySelector('.Question');
-  question.classList.toggle('Question_open');
+  function handleGoToTop(event) {
+    window.scrollTo(pageXOffset, 0);
+  }
 
-  const answer = questionWithAnswer.querySelector('.Answer');
-  answer.classList.toggle('Answer_open');
+  window.addEventListener('scroll', handleShowButton);
+  goUpButton.addEventListener('click', handleGoToTop);
+  
+}
 
-}   
-
-questions.addEventListener('click', handleQuestionsToggle);
+goUpButton();
