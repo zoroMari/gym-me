44dce1efb0d0ab9f4a47cd8e97b2e88484f53b11
Mariia Moroz <98117895+zoroMari@users.noreply.github.com>
2022-01-25 14:19:13 +0200

    Feature/Burger menu (#8)
    * feature/add scroll for  menu on desktop
    
    * Feature/Go up button
    
    * Feature/Create burger menu
    
    * Feature/Autoclose burger menu
    
    * Autoclose chahtyjtyfjgjg
    
    * finished menu

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
-const questions = document.querySelector('.Questions');
+function questionsToggle() {
+  const questions = document.querySelector('.Questions');
+
+  function handleQuestionsToggle(event) {
+    const questionWithAnswer = event.target.closest('.QuestionWithAnswer'); 
+
+    if (!questionWithAnswer) return;
+
+    const question = questionWithAnswer.querySelector('.Question');
+    question.classList.toggle('Question_open');
+
+    const answer = questionWithAnswer.querySelector('.Answer');
+    answer.classList.toggle('Answer_open');
+  }   
+
+  questions.addEventListener('click', handleQuestionsToggle);
+}
+
+questionsToggle();
 
 
-function handleQuestionsToggle(event) {
-  const questionWithAnswer = event.target.closest('.QuestionWithAnswer'); 
+function goUpButton() {
+  const goUpButton = document.getElementById('goUp');
 
-  if (!questionWithAnswer) return;
+  function handleShowButton() {
+    pageYOffset > document.documentElement.clientHeight ? goUpButton.style.display = 'block' :
+    goUpButton.style.display = '';
+  }
 
-  const question = questionWithAnswer.querySelector('.Question');
-  question.classList.toggle('Question_open');
+  function handleGoToTop(event) {
+    window.scrollTo(pageXOffset, 0);
+  }
 
-  const answer = questionWithAnswer.querySelector('.Answer');
-  answer.classList.toggle('Answer_open');
+  window.addEventListener('scroll', handleShowButton);
+  goUpButton.addEventListener('click', handleGoToTop);
+}
 
-}   
+goUpButton();
 
-questions.addEventListener('click', handleQuestionsToggle);
+function burgerMenu() {
+  const burgerOpen = document.querySelector('.Header .BurgerMenu');
+  const burgerClose = document.querySelector('.HeaderMobile .BurgerMenu')
+  const menuMobile = document.querySelector('.HeaderMobile');
+  const menuElements = document.querySelector('.HeaderMobile ul');
+
+  function handleOpenMenu() {
+    menuMobile.style.display = 'block';
+  }
+
+  function handleCloseMenu() {
+    menuMobile.style.display = '';
+  }
+
+  function handleAutoCloseMenu(event) {
+    const menuElem = event.target.closest('li'); 
+
+    if (!menuElem) return;
+    if (document.documentElement.clientWidth > 800) return;
+
+    menuMobile.style.display = 'none';
+  }
+
+  burgerOpen.addEventListener('click', handleOpenMenu);
+  burgerClose.addEventListener('click', handleCloseMenu);
+  menuElements.addEventListener('click', handleAutoCloseMenu);
+}
+
+burgerMenu();
