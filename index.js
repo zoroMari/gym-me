77cd4f95c6e4d731dc1cd166963bce39293dc635
Mariia Moroz <98117895+zoroMari@users.noreply.github.com>
2022-02-17 16:21:51 +0200

    Feature/gallery (#9)
    * add new files lgallery into folder
    
    * Initialize new files
    
    * create gallery fot training programs
    
    * Add galley for community
    
    * create comments gallery for desktop
    
    * comments gallery with mobile
    
    * comments gallery done

--- index.js
+++ index.js
@@ -66,3 +66,74 @@ function burgerMenu() {
 }
 
 burgerMenu();
+
+
+function carouselComments() {
+  const commentsDesk = document.querySelector('.Community .Сomments');
+  const commentsWrapperDesk = document.querySelector('.Community-Desktop .Comments-Wrapper');
+  const commentsWrapperMob = document.querySelector('.Community-Mobile .Comments-Wrapper');
+  const comment = document.querySelector('.Community .Сomment');
+  const arrowNextDesk = document.querySelector('.Community-Desktop .Arrow_next');
+  const arrowPrevDesk = document.querySelector('.Community-Desktop .Arrow_previous');
+  const arrowNextMob = document.querySelector('.Community-Mobile .Arrow_next');
+  const arrowPrevMob = document.querySelector('.Community-Mobile .Arrow_previous');
+  let width = (document.documentElement.clientWidth > 800) ? 360 : 300;
+  const count = 4;
+  const margin = 32;
+  const widthOfComments = count * (width + margin); // видимое количество изображений
+
+  let position = 0; 
+  let positionMax;
+  let positionMin;// положение ленты прокрутки
+
+
+  arrowPrevDesk.onclick = function() { 
+    position += width + margin;
+    position = Math.min(position, 0)
+    commentsWrapperDesk.style.marginLeft = position + 'px';   
+    arrowNextDesk.querySelector('path').style.fill = '#131316';
+    if (position === 0) {
+      arrowPrevDesk.querySelector('path').style.fill = '';
+    } 
+  };
+  
+
+  arrowPrevMob.onclick = function() {
+    position += width;
+    positionMin = (widthOfComments - document.documentElement.clientWidth);
+    position = Math.min(position, positionMin)
+    commentsWrapperMob.style.marginLeft = position + 'px';   
+    arrowNextMob.querySelector('path').style.fill = '#131316';
+    if (position === positionMin) {
+      arrowPrevMob.querySelector('path').style.fill = 'var(--color-grey_light)';
+    } 
+  };
+
+
+  arrowNextDesk.onclick = function() {
+    position -= width + margin;
+    positionMax = (commentsDesk.clientWidth - widthOfComments);
+    position = Math.max(position, positionMax);
+    commentsWrapperDesk.style.marginLeft = position + 'px';
+    arrowPrevDesk.querySelector('path').style.fill = '#131316';
+
+    if (position === positionMax) {
+      arrowNextDesk.querySelector('path').style.fill = 'var(--color-grey_light)';
+    }
+  }; 
+
+
+  arrowNextMob.onclick = function() {
+    position -= width;
+    positionMax = -(widthOfComments - document.documentElement.clientWidth);
+    position = Math.max(position, positionMax);
+    commentsWrapperMob.style.marginLeft = position + 'px';
+    arrowPrevMob.querySelector('path').style.fill = '#131316';
+  
+    if (position === positionMax) {
+      arrowNextMob.querySelector('path').style.fill = 'var(--color-grey_light)';
+    }
+  }; 
+}
+
+carouselComments();
